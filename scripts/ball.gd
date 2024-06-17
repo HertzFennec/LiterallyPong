2452638bae47c1841723a4f1e396186e834fcc90
HertzFennec <cooldude0238@gmail.com>
2024-06-17 18:48:46 -0500

    Improving ball physics
    Trying to work out a physics system with the ball that will allow it to bounce off of walls. Not fully working yet.
    
    MAJOR FIX: Collisions are now working with all objects!!!!!

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,18 +1,20 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 var direction = Vector2.ZERO
-func _ready():
+func _ready(): #picks a random direction for the ball upon opening the scene/game, and sets an intial velocity Vector
 	direction.x = [-1, 1].pick_random()
 	direction.y = [-1, 1].pick_random()
 	velocity = Vector2(direction)
 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
-	if collision:
-		velocity = velocity.bounce(collision.get_normal())
-		direction = Vector2(-direction.x, -direction.y)
+	if collision: #all this does is get what collided with the ball and adjusts it's direction accordingly
+		var hitObject = str(collision.get_collider())
+		if hitObject.contains("Top") or hitObject.contains("Bottom") == true:
+			direction = Vector2(direction.x, -direction.y)
+		else:
+			direction = Vector2(-direction.x, direction.y)
 	else:
 		velocity = direction * SPEED
 	position += velocity * delta
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 const SPEED = 350.0
-@export var friction = 0.05
+@export var friction = 0.2
 
 func _physics_process(delta):
 	var direction = get_axis()
