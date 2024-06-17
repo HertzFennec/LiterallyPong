014399d0c4cd80987294764f0476791bc89fa57c
HertzFennec <cooldude0238@gmail.com>
2024-06-16 19:16:18 -0500

    initial player script
    Script that controls the player.
    
    As of this commit, collisions are not working for an unknown reason.

---
+++ scripts/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+const SPEED = 350.0
+@export var friction = 0.05
+
+func _physics_process(delta):
+	var direction = get_axis()
+	var currentVelocity = get_real_velocity()
+	match direction:
+		-1, 1: #ok, so apparently OR will cause it to only choose one or the other. use "," instead.
+			velocity = Vector2(0, direction * SPEED)
+		0:
+			velocity = lerp(currentVelocity, Vector2.ZERO, friction)
+	position += velocity * delta
+	print(direction)
+	move_and_slide()
+	
+func get_axis(): #function called to get direction based on input
+	if Input.is_action_just_pressed("up"):
+		return -1
+	elif Input.is_action_just_pressed("down"):
+		return 1
+	else:
+		return 0
