80058ec20b799ff1b72dd88f7a47422c3e731538
HertzFennec <cooldude0238@gmail.com>
2024-06-21 14:57:38 -0500

    slight tweaks
    opponent paddle now SOMEWHAT WORKS. can't keep pace with the ball it's y position is slightly higher though

--- scripts/opponent.gd
+++ scripts/opponent.gd
@@ -3,29 +3,26 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 @export var friction = 0.2
-var ballPosition = Vector2(568, 312)
-@export var rate = 50
+var ballPosition = Vector2(0, 0)
+@export var rate = 20
 
 func _physics_process(delta):
 	var direction = get_axis(ballPosition)
 	var currentVelocity = get_real_velocity()
 	match direction:
-		-1, 1: #ok, so apparently OR will cause it to only choose one or the other. use "," instead.
-			velocity.y = velocity.move_toward(ballPosition, rate).y
-		0:
-			velocity.y = lerp(currentVelocity, Vector2.ZERO, friction)
+		-1: #ok, so apparently OR will cause it to only choose one or the other. use "," instead.
+			velocity.y = velocity.move_toward(ballPosition, rate).y * direction 
+		1:
+			velocity.y = velocity.move_toward(ballPosition, rate).y * direction 
 	position += velocity * delta
-	print(ballPosition)
-	print(position)
+	print(direction)
 	move_and_slide()
 
 func get_axis(ball_pos):
-	if ball_pos < position:
+	if ball_pos.y < position.y:
 		return -1
-	if ball_pos > position:
+	if ball_pos.y > position.y:
 		return 1
-	else:
-		return 0
 
 func _on_ball_tell_opponent(currentPosition):
 	ballPosition = currentPosition
